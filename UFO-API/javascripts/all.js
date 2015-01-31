907f40e5ab431d23ab911ab13e311e58583f4819
Yuan Cheung <zhangyuan@users.noreply.github.com>
2015-01-31 21:00:23 +0800

    frontend: add products controller

---
+++ UFO-API/javascripts/all.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module('app', []);
+
+app.controller("ProductsController", [
+    "$scope", function($scope) {
+        $scope.products = [
+            {name: "Subway"},
+            { name: "Burger King" }
+        ];
+    }
+]);
+
+
